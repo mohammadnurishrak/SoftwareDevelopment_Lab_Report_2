880047ddd8b83ca7a0164bc72d88e80d75af9fd9
Mohammad Nur Ishrak <ishrak003323@gmail.com>
2026-05-01 23:35:35 +0600

    Set-1 Done

---
+++ Set-1 (Beginner)/Two_C251073.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Two_C251073{
+	public static void main(String[] args){
+		Scanner helper = new Scanner(System.in);
+		System.out.print("Your Name: ");
+		String name = helper.nextLine();
+
+		System.out.println("Hello, " + name);
+	}
+}
